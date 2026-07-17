872f03d893e277238b640cda26302d0b808f9e73
eshankmaheshwari-star <eshankmaheshwari@gmail.com>
2026-07-17 20:42:16 +0530

    Time: 19 ms (80.1%), Space: 14.1 MB (26.55%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -16,8 +16,19 @@ int dpz(int i,int j,int l,int r){
         m=nm;
         n=cuts.size();
         sort(cuts.begin(),cuts.end());
-        a=cuts;
-        dp.assign(n,vector<int>(n,-1));
-        return dpz(0,n-1,0,m);
+        a.push_back(0);
+        for(int x:cuts) a.push_back(x);
+        a.push_back(m);
+        dp.assign(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                int ans=INT_MAX;
+                for(int k=i;k<=j;k++){
+                    ans=min(ans,dp[i][k-1]+dp[k+1][j]+a[j+1]-a[i-1]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[1][n];
     }
 };
