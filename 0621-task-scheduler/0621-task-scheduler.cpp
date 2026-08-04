e2a30a625e07f54e880881b623dbd2d7f77c12ca
eshankmaheshwari-star <eshankmaheshwari@gmail.com>
2026-08-04 22:47:48 +0530

    Time: 39 ms (30.64%), Space: 47.7 MB (20.64%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,24 +3,44 @@ public:
     int leastInterval(vector<char>& t, int n) {
         vector<int> mp(26,0);
         for(char c:t) mp[c-'A']++;
-        sort(mp.rbegin(),mp.rend());
+        priority_queue<int> pq;
+        for(int i=0;i<26;i++){ 
+            if(mp[i]>0) pq.push(mp[i]);
+        }
         int time=0;
-            while (mp[0] > 0) {
-                int rlx = 0;
-
-                for (int i = 0; i < 26 && rlx < n + 1; i++) {
-                    if (mp[i] > 0) {
-                        mp[i]--;
-                        rlx++;
-                        time++;
-                    }
-                }
-
-                sort(mp.rbegin(), mp.rend());
-
-                if (mp[0] > 0)
-                    time += (n + 1 - rlx);
+        while(!pq.empty()){
+            int rlx=0;
+            vector<int> temp;
+            for(int i=0;i<=n;i++){
+                if(pq.empty()) break;
+                int x=pq.top();
+                pq.pop();
+                rlx++;
+                time++;
+                x--;                
+                if(x>0) temp.push_back(x);
             }
+            for(int x:temp) pq.push(x);
+            if(!pq.empty()) time+=(n+1-rlx);
+        }
+        // sort(mp.rbegin(),mp.rend());
+        // int time=0;
+        //     while (mp[0] > 0) {
+        //         int rlx = 0;
+
+        //         for (int i = 0; i < 26 && rlx < n + 1; i++) {
+        //             if (mp[i] > 0) {
+        //                 mp[i]--;
+        //                 rlx++;
+        //                 time++;
+        //             }
+        //         }
+
+        //         sort(mp.rbegin(), mp.rend());
+
+        //         if (mp[0] > 0)
+        //             time += (n + 1 - rlx);
+        //     }
         return time;
     }
 };
