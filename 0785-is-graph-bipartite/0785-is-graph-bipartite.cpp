2a08b68397b255f5de0355e1a0f1db39b671bfb8
eshankmaheshwari-star <eshankmaheshwari@gmail.com>
2026-08-10 19:13:18 +0530

    Time: 0 ms (100%), Space: 17.5 MB (58.91%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -3,26 +3,41 @@ public:
     bool isBipartite(vector<vector<int>>& adj) {
         int n=adj.size();
         vector<int> vis(n,0);
-        function<bool(int)> bfs=[&](int i){
-            queue<int> q;
-            vis[i]=1;
-            q.push(i);
-            while(!q.empty()){
-                int node=q.front();
-                q.pop();
-                for(int x:adj[node]){
+        // function<bool(int)> bfs=[&](int i){
+        //     queue<int> q;
+        //     vis[i]=1;
+        //     q.push(i);
+        //     while(!q.empty()){
+        //         int node=q.front();
+        //         q.pop();
+        //         for(int x:adj[node]){
+        //             if(!vis[x]){
+        //                 q.push(x);
+        //                 vis[x]=-vis[node];
+        //             }
+        //             else if(vis[node]==vis[x]) return false;
+        //         }
+        //     }
+        //     return true;
+        // };
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]){
+        //         if(!bfs(i)) return false;
+        //     }
+        // }
+        function<bool(int,int)> dfs=[&](int i,int j){
+            vis[i]=j;
+                for(int x:adj[i]){
                     if(!vis[x]){
-                        q.push(x);
-                        vis[x]=-vis[node];
+                        if(!dfs(x,-j)) return false;
                     }
-                    else if(vis[node]==vis[x]) return false;
+                    else if(vis[i]==vis[x]) return false;
                 }
-            }
             return true;
         };
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                if(!bfs(i)) return false;
+                if(!dfs(i,1)) return false;
             }
         }
         return true;
